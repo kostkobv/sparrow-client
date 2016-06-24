0fda225ba8653bfc88d3421b8f3b83eeebb4d662
Bohdan Kostko <bohdan.kostko@gmail.com>
2016-06-24 11:10:13 +0200

    fix(socket): added socket support

--- src/js/socket.js
+++ src/js/socket.js
@@ -3,25 +3,46 @@
 const config = require('./config.json');
 const Kalm = require('./vendor/kalm');
 const ws = require('./vendor/kalm-websocket');
+const grid = require('./grid');
 
+/**
+ * Registers socket client
+ */
 function registerClient() {
 	Kalm.adapters.register('ws', ws);
 
 	return new Kalm.Client({
-		hostname: config.HOSTNAME,
-		port: config.PORT,
-		adapter: 'ws'
+		hostname: config.SOCKET_HOSTNAME,
+		port: config.SOCKET_PORT,
+		adapter: 'ws',
+		percentPosition: true,
+		masonry: {
+			columnWidth: config.GRID_ITEM_SELECTOR
+		}
 	});
 }
 
+/**
+ * Subscribes socket client to receive tweets
+ *
+ * @param client - socket client instance
+ */
 function subscribe(client) {
-	client.subscribe('tweets', (data) => {
-		console.log(data);
+	client.subscribe(config.SOCKET_TWEETS_CHANNEL, (tweets) => {
+		grid.addItems(tweets);
+	});
+
+	client.subscribe(config.SOCKET_TWEET_CHANNEL, (tweet) => {
+		grid.addItem(tweet);
 	});
 }
 
+/**
+ * Inits socket service
+ */
 export function init() {
 	const client = registerClient();
 
+	grid.init();
 	subscribe(client);
 }
